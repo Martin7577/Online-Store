57957d41016b0120ff07ada1578cf9aba3f89455
Timur <marteen.free@gmail.com>
2023-12-27 15:56:15 +0200

    добавлены фильтры и теги, добавлена пагинация на основной странице

---
+++ project/simpleapp/templatetags/custom_filters.py
@@ -0,0 +1,17 @@
+from django import template
+
+
+register = template.Library()
+
+
+CURRENCIES_SYMBOLS = {
+   'rub': '₽',
+   'usd': '$',
+}
+
+@register.filter()
+def currency(value, code='rub'):
+
+   postfix = CURRENCIES_SYMBOLS[code]
+
+   return f'{value} {postfix}'
